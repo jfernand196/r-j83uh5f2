28c2d6afeb6530c450046643045501904249dcc1
jfernand <jfernand196@gmail.com>
2022-01-24 17:22:54 -0500

    Chore: count word component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+// class App extends Component {
+//   constructor(){
+//     super();
+//    this.state = {count: 0}
+//   }
+//   handleChange(event){
+//     this.setState({count: event.target.value.length})
+//     console.log(this.state.count)
+//   }
+//   render() {
+//     return (
+//       <div className="container">
+//         <textarea rows="3" onChange={this.handleChange}></textarea>
+//         <div className="counter">{this.state.value.length}</div>
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => {
+
+  const[count, setCount] = useState(0)
+
+
+
+  return(
+
+<div className="container">
+        <textarea rows="3" onChange={e => setCount(e.target.value.length)}></textarea>
+        <div className="counter">{count}</div>
       </div>
-    );
-  }
+
+  )
+
+
 }
 
 export default App;
